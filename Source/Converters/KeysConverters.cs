323e307468f4a219e91c965659903ed430037594
coffeeSurplus <lwhchang@gmail.com>
2024-08-22 10:22:59 +0100

    - improved visibility of black keys held down

--- Source/Converters/KeysConverters.cs
+++ Source/Converters/KeysConverters.cs
@@ -6,7 +6,7 @@ namespace MidiPiano.Source.Converters;
 
 internal class KeysBlackConverter : IValueConverter
 {
-	public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => new SolidColorBrush((Color)ColorConverter.ConvertFromString((bool)value ? "#19223F" : "#000000"));
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => new SolidColorBrush((Color)ColorConverter.ConvertFromString((bool)value ? "#374A8A" : "#000000"));
 	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => Binding.DoNothing;
 }
 
